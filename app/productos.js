5ba1d0733519bed5d1a0acca9aa0fddf6110d167
marcosinca-istea <marcos.inca@istea.com.ar>
2026-05-10 12:28:58 -0300

    Add contacto

--- app/productos.js
+++ app/productos.js
@@ -1,4 +1,3 @@
-// Usamos const para el array como pidió el profe
 const baseDeDatosCartas = [
     {
         id: "stardust",
@@ -21,6 +20,16 @@ const baseDeDatosCartas = [
         nivel: 8
     },
     {
+        id: "black-rose",
+        nombre: "Black Rose Dragon",
+        precio: 42000,
+        imagen: "img/black-rose.webp",
+        edicion: "Original Synchro",
+        descripcion: "El dragón de Akiza Izinski. Ideal para limpiar el campo en un instante. Near Mint.",
+        tipo: "Dragón / Sincronía / Efecto",
+        nivel: 7
+    },
+    {
         id: "junk-warrior",
         nombre: "Junk Warrior",
         precio: 15000,
@@ -29,5 +38,45 @@ const baseDeDatosCartas = [
         descripcion: "La carta insignia de Yusei en los primeros compases de la serie. Detalles leves.",
         tipo: "Guerrero / Sincronía / Efecto",
         nivel: 5
+    },
+    {
+        id: "shooting-star",
+        nombre: "Shooting Star Dragon",
+        precio: 85000,
+        imagen: "img/shooting-star-dragon.webp",
+        edicion: "Ghost Rare",
+        descripcion: "Evolución directa de Stardust Dragon mediante Accel Synchro. Estado impecable.",
+        tipo: "Dragón / Sincronía / Efecto",
+        nivel: 10
+    },
+    {
+        id: "red-nova",
+        nombre: "Red Nova Dragon",
+        precio: 70000,
+        imagen: "img/red-nova-dragon.webp",
+        edicion: "Secret Rare",
+        descripcion: "El dragón más fuerte de Jack Atlas tras absorber el poder del Red Nova. Mint condition.",
+        tipo: "Dragón / Sincronía / Efecto",
+        nivel: 12
+    },
+    {
+        id: "moonlight-rose",
+        nombre: "Moonlight Rose Dragon",
+        precio: 25000,
+        imagen: "img/black-rose-moonlight-dragon.webp",
+        edicion: "Ultra Rare",
+        descripcion: "Una versión alternativa del Black Rose Dragon, perfecta para controlar el campo rival.",
+        tipo: "Dragón / Sincronía / Efecto",
+        nivel: 7
+    },
+    {
+        id: "nitro-warrior",
+        nombre: "Nitro Warrior",
+        precio: 8000,
+        imagen: "img/nitro-warrior.webp",
+        edicion: "Super Rare",
+        descripcion: "Guerrero de Yusei enfocado en potenciar su ataque tras usar cartas mágicas. Detalles leves.",
+        tipo: "Guerrero / Sincronía / Efecto",
+        nivel: 7
     }
 ];
